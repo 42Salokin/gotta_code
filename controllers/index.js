b5874939ce68bc2fe6ac4a46a0e93619288595cc
Salokin <nicholas.dpoul@gmail.com>
2024-05-13 20:02:30 -0400

    made main page display

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+// const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
 router.use((req, res) => {
     res.send("<h1>Wrong Route!</h1>")
