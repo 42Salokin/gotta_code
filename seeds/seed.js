cefa0d938dd140b1fb20e769e30062dc8ec4755d
Salokin <nicholas.dpoul@gmail.com>
2024-05-13 21:08:23 -0400

    made a succesful GET request to PokeAPI and narrowed the data down to id, name, and type

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,6 +3,23 @@ const sequelize = require('../config/connection');
 // const pokedex = require('pokedex2.js');
 // const dex = pokedex();
 
+const logout = async () => {
+    const pokeId = 1;
+    const queryURL = `https://pokeapi.co/api/v2/pokemon/${pokeId}/`
+    const response = await fetch(queryURL, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data.id, data.name, data.types[0].type.name, data.types[1].type.name); // Log the JSON data
+        } else {
+      alert(response.statusText);
+    }
+  };
+
+  logout();
 // const { Pokes, Evolutions } = require('../models');
 
 // const seedDatabase = async () => {
